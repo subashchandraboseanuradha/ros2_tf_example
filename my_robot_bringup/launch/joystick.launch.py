34d64f2b8944159af23fe7114c97535ee389b93f
subash chandra bose a <subashchandrabose2003@gmail.com>
2025-09-08 20:19:31 +0530

    added nav2 , slam praram , maps

--- my_robot_bringup/launch/joystick.launch.py
+++ my_robot_bringup/launch/joystick.launch.py
@@ -9,7 +9,7 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    joy_params = os.path.join(get_package_share_directory('my_bot'),'config','joystick.yaml')
+    joy_params = os.path.join(get_package_share_directory('my_robot_bringup'),'config','joystick.yaml')
 
     # Print a message about the time source being used
     log_time_source = LogInfo(
